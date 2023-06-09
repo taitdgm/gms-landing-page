a39dad0ac30a8352eabfc3d6c714ddc8cc718535
taitd-gms <136054433+taitdgm@users.noreply.github.com>
2023-06-09 18:32:13 +0700

    feat: change header styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,38 @@ window.addEventListener("load", () => {
 });
 
 /**
+ * Header
+ */
+
+const header = document.querySelector("[data-header]");
+
+const headerActive = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
+
+window.addEventListener("scroll", headerActive);
+
+/**
+ * Navbar toggler for mobile devices
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+/**
  * SLIDER
  */
 const sliders = document.querySelectorAll("[data-slider]");
